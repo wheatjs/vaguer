6606c89172f28cf512b5ace952e6c890536a9bdf
wheatjs <jacobrclevenger@gmail.com>
2021-10-03 03:05:38 -0400

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 
-declare const __dirname: string
-
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
